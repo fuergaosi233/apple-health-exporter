7369fdc2b51116c0806715d6b582f05c346d9eb7
Holegots <fuergaosi@gmail.com>
2023-09-16 03:31:00 +0800

    ✨: Change chunk_time_interval time

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
 from sqlalchemy_timescaledb.dialect import TimescaledbDDLCompiler
 from sqlalchemy import DDL
+
+
 @staticmethod
 def ddl_hypertable(table_name, hypertable):
-    time_column_name = hypertable['time_column_name']
+    time_column_name = hypertable["time_column_name"]
     partitioning_column = hypertable.get("partitioning_column", None)
     number_partitions = hypertable.get("number_partitions")
-    chunk_time_interval = hypertable.get('chunk_time_interval', '7 days')
+    chunk_time_interval = hypertable.get("chunk_time_interval", "30 days")
 
     if isinstance(chunk_time_interval, str):
         if chunk_time_interval.isdigit():
@@ -14,8 +16,10 @@ def ddl_hypertable(table_name, hypertable):
             chunk_time_interval = f"INTERVAL '{chunk_time_interval}'"
 
     return DDL(
-        f"CREATE UNIQUE INDEX idx_{partitioning_column}_{time_column_name} ON {table_name}({partitioning_column}, {time_column_name});" if partitioning_column else "" +
-        f"""
+        f"CREATE UNIQUE INDEX idx_{partitioning_column}_{time_column_name} ON {table_name}({partitioning_column}, {time_column_name});"
+        if partitioning_column
+        else ""
+        + f"""
         SELECT create_hypertable(
             '{table_name}',
             '{time_column_name}',
@@ -26,4 +30,6 @@ def ddl_hypertable(table_name, hypertable):
         );
         """
     )
+
+
 TimescaledbDDLCompiler.ddl_hypertable = ddl_hypertable
